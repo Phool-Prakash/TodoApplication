778e9d1ab95c024c0e9104969f820a771cba36d8
Phool Prakash <phoolprakash62@gmail.com>
2024-07-17 01:00:26 +0530

    A

--- lib/screens/todoScreen.dart
+++ lib/screens/todoScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:todo_aap/customExtension/customExtension.dart';
+import 'package:todo_aap/theme/myColors.dart';
 
 class TodoScreen extends StatefulWidget {
   const TodoScreen({super.key});
@@ -11,6 +12,16 @@ class TodoScreen extends StatefulWidget {
 class _TodoScreenState extends State<TodoScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: ColorTheme.whiteBold,
+        actions: [
+          const CircleAvatar(
+            backgroundImage: AssetImage('assets/profilepic.png'),
+          ).padAll(8)
+        ],
+      ),
+    );
   }
 }
